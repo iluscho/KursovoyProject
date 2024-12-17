3f492c975e294e91b3040f7664cce3926a62093c
asfasdfgasdfasdf@outlook.com <asfasdfgasdfasdf@outlook.com>
2024-12-17 15:22:02 +0500

    nu hz

--- KursovoyProject/AddVisitWindow.xaml.cs
+++ KursovoyProject/AddVisitWindow.xaml.cs
@@ -1,29 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace KursovoyProject
 {
     public partial class AddVisitWindow : Window
     {
         private CarVisits _visit;
-        int _carID;
+        private int _carID;
+
         public AddVisitWindow(CarVisits visit, int carID)
         {
             InitializeComponent();
             _visit = visit;
             _carID = carID;
-            CarIDTextBox.Text = Convert.ToString(_carID);
+            CarIDTextBox.Text = _carID.ToString();
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
@@ -32,21 +23,38 @@ namespace KursovoyProject
             {
                 using (var context = new IlyaServiceTemp1Entities())
                 {
-                    var car = context.ClientCars.FirstOrDefault(c => c.CarID == _visit.CarID);
-                    car.VIN = CarIDTextBox.Text;
-                    //_visit.EmpID = EmpIDTextBox.Text;
-                    //_visit.VisitDate = VisitDateTextBox.Text;
-                    //_visit.Description = DescTextBox.Text;
-                    //_visit.Cost = CostTextBox.Text;
-                    _visit.Status = StatusTextBox.Text;
-                    //_visit.RepairParts = RepairPartsListBox;
+                    var newCarVisit = new CarVisits
+                    {
+                        CarID = _carID,
+                        Description = DescTextBox.Text,
+                        Cost = 0,
+                        Status = "In Progress",
+                        VisitDate = DateTime.Now
+                    };
 
+                    // Попробуем преобразовать EmpID
+                    if (int.TryParse(EmpIDTextBox.Text, out int empID))
+                    {
+                        newCarVisit.EmpID = empID;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Введите корректный номер сотрудника.");
+                        return;
+                    }
+
+                    // Добавляем новую запись в таблицу CarVisits
+                    context.CarVisits.Add(newCarVisit);
+                    context.SaveChanges();
+
+                    MessageBox.Show("Запись успешно добавлена!");
                     DialogResult = true;
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Ошибка добавления записи: {ex.Message}");
+                string fullError = ex.InnerException != null ? ex.InnerException.Message : ex.Message;
+                MessageBox.Show($"Ошибка добавления записи AddVisitWindow: {fullError}");
             }
         }
     }
